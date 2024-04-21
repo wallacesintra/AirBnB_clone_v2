43455a82de7b198e82f137e31792088c62389361
wallacesintra <wallvxwallace962@gmail.com>
2024-04-21 20:57:39 +0300

    even or not

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+Script that runs an app with Flask framework:
+Routes:
+    * /: display “Hello HBNB!”
+    * /hbnb: display “HBNB”
+    * /c/<text>: display “C ”, and value of the text variable
+    * /python/(<text>): display “Python ”,and the value of the text variable
+        The default value of text is “is cool”
+    * /number/<n>: display “n is a number” only if n is an integer
+    * /number_template/<n>: display a HTML page only if n is an integer:
+        H1 tag: “Number: n” inside the tag BODY
+    * /number_odd_or_even/<n>: display a HTML page only if n is an integer:
+        H1 tag: “Number: n is even|odd” inside the tag BODY
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """/ route """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """  /hbnb route """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """/c/<text> route """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """/python/<text> route """
+    if text is not 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """/number/<n> route """
+    return "%d is a number" % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """/number_template/<n> route """
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """/number_template/<n> route """
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
