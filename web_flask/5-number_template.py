40b18b33b993b515fbde50db9123c335aad5381c
wallacesintra <wallvxwallace962@gmail.com>
2024-04-21 20:48:22 +0300

    Refactor Flask routes in web_flask/5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -22,26 +22,26 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Function called with / route """
+    """/ route """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Function called with /hbnb route """
+    """/hbnb route """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """ Function called with /c/<text> route """
+    """/c/<text> route """
     return 'C %s' % text.replace('_', ' ')
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is cool'):
-    """ Function called with /python/<text> route """
+    """/python/<text> route """
     if text is not 'is cool':
         text = text.replace('_', ' ')
     return 'Python %s' % text
@@ -49,13 +49,13 @@ def python_text(text='is cool'):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """ Function called with /number/<n> route """
+    """/number/<n> route """
     return "%d is a number" % n
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """ Function called with /number_template/<n> route """
+    """/number_template/<n> route """
     return render_template('5-number.html', number=n)
 
 
